d8ff8b15c2a9efc08c5d74fa3a77f662fe2b474d
AlbertWebs <albertmuhatia@gmail.com>
2022-07-19 16:22:35 +0300

    Update

--- resources/views/front/master.blade.php
+++ resources/views/front/master.blade.php
@@ -3,7 +3,7 @@
 
 <head>
 <meta charset="utf-8">
-<title>Royal Tech Template | Laptops For Hire | IT Solutions in Nairobi</title>
+<title>Laptops For Hire | Royal Tech Template | IT Solutions in Nairobi</title>
 <!-- Responsive -->
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
