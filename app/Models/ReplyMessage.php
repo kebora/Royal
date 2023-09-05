caa931b435af967071b1aac10c3ae833f560ed95
Albert Muhatia <albertmuhatia@gmail.com>
2023-09-05 14:41:00 +0300

    Update

--- app/Models/ReplyMessage.php
+++ app/Models/ReplyMessage.php
@@ -148,7 +148,7 @@ class ReplyMessage extends Model
 
         Mail::send('mailContact', $data, function($message) use ($subject,$FromVariable,$FromVariableName,$toVariable,$toVariableName,$email){
             $message->from($FromVariable , $FromVariableName);
-            $message->to($toVariable, $toVariableName)->cc('albertmuhatia@gmail.com')->replyto($email)->subject($subject);
+            $message->to($toVariable, $toVariableName)->cc('sales@royaltech.co.ke')->bcc('albertmuhatia@gmail.com')->replyto($email)->subject($subject);
         });
     }
 
